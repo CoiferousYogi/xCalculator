6d4d95ee2409735d045ea0993321ea5f25496fb7
Ritwik Pai <ritwikpai777@gmail.com>
2024-12-02 23:35:35 +0530

    Pushing the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  // function to execute when a button is clicked
+  const buttonClick = (value) => {
+    setInput((prev) => prev + value);
+  };
+
+  const handleCalculations = () => {
+    try {
+      const evalResult = eval(input);
+
+      // check for the edge cases : division by 0 and NaN
+      if (input.includes("0/0") && !evalResult) {
+        setResult("NaN");
+      } else if (input.includes("/0") && !evalResult) {
+        setResult("Infinity");
+      } else if (isNaN(evalResult)) {
+        setResult("Error");
+      } else {
+        setResult(evalResult.toString());
+      }
+    } catch {
+      setResult("Error");
+    }
+  };
+
+  // function for clear
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="mainHeading">React Calculator</h1>
+      <div className="calculator">
+        {/* Input area */}
+        <input className="input-field" type="text" value={input} readOnly />
+        {/* Result Display */}
+        <div className="result">{result}</div>
+        {/* buttons */}
+        <div className="buttons">
+          <button onClick={() => buttonClick("7")}>7</button>
+          <button onClick={() => buttonClick("8")}>8</button>
+          <button onClick={() => buttonClick("9")}>9</button>
+          <button onClick={() => buttonClick("+")}>+</button>
+          <button onClick={() => buttonClick("4")}>4</button>
+          <button onClick={() => buttonClick("5")}>5</button>
+          <button onClick={() => buttonClick("6")}>6</button>
+          <button onClick={() => buttonClick("-")}>-</button>
+          <button onClick={() => buttonClick("1")}>1</button>
+          <button onClick={() => buttonClick("2")}>2</button>
+          <button onClick={() => buttonClick("3")}>3</button>
+          <button onClick={() => buttonClick("*")}>*</button>
+          <button onClick={handleClear}>C</button>
+          <button onClick={() => buttonClick("0")}>0</button>
+          <button onClick={handleCalculations}>=</button>
+          <button onClick={() => buttonClick("/")}>/</button>
+        </div>
+      </div>
     </div>
   );
 }
